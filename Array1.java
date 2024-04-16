657aec444d8a7c60d0edd726cea7fa2b9cbb7fe4
Jayraj <123743575+Jayraj2304@users.noreply.github.com>
2024-04-16 13:47:29 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Array1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr1 = new ArrayList<>(5);
+        arr1.add(5);
+        arr1.add(0,10);
+        System.out.println(arr1);
+    }
+    
+}
